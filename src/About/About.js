56e74ebee5cc67ba7b73c0e11128cd16faa94ac2
Atharv Phadnis <athiphadnis@gmail.com>
2021-09-05 21:18:49 +0530

    Fixed 'About' links

--- src/About/About.js
+++ src/About/About.js
@@ -10,10 +10,10 @@ export default function About() {
 		<div className="about">
 			<Coronavirus />
 			<h1 className="mt-3">Covid-19 India Tracker</h1><br />
-			<h3>Made by <u><i><a href="github.com/Athi223" target="_blank" rel="noopener noreferrer">Athi223</a></i></u></h3><br />
+			<h3>Made by <u><i><a href="https://github.com/Athi223" target="_blank" rel="noopener noreferrer">Athi223</a></i></u></h3><br />
 			<h5>Credits: 
 				<ul className="list">
-					<li><u><a href="https://api.covid19india.org" target="_blank" rel="noopener noreferrer">Covid-19 India API</a></u></li>
+					<li><u><a href="https://data.covid19india.org" target="_blank" rel="noopener noreferrer">Covid-19 India API</a></u></li>
 					<li><u><a href="https://covid19api.com" target="_blank" rel="noopener noreferrer">Covid-19 API</a></u></li>
 				</ul>
 			</h5>
